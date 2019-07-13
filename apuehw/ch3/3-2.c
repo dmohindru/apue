29e98c19caa30bd0852af114bb20c35f04e39267
Dhruv Mohindru <dmohindru@gmail.com>
2019-07-13 09:53:37 +0530

    Solved ex 3-2

--- apuehw/ch3/3-2.c
+++ apuehw/ch3/3-2.c
@@ -4,10 +4,13 @@
    Description:     own implementation of dup2 function without using fcntl function 
 */
 
-#include <unistd.h>
+#include "../include/apue.h"
 #include <stdio.h>
 #include <fcntl.h>
-#include <stdlib.h>
+#include <errno.h>
+#include <unistd.h>
+
+#define	FILE_MODE	(S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH)
 
 
 int mydup2(int fd, int fd2);  /* our own implementation of dup2 function */
@@ -17,20 +20,78 @@ main(void)
 {
     int fd, fd1;
 
-    fd = open("mytest.file", O_RDWR| O_CREAT);
+    fd = open("mytest.file", O_RDWR| O_CREAT, FILE_MODE);
 
-    fd1 = dup2(fd, 12);
+    fd1 = mydup2(fd, 6);
 
+    printf("Descriptor using dup2 function\n");
+    printf("fd: %d\n", fd);
+    printf("fd1: %d\n", fd1);
+    
+    /* Using dup function */
+    /*  fd1 = dup(fd);
+
+    printf("Descriptor using dup function\n");
     printf("fd: %d\n", fd);
     printf("fd1: %d\n", fd1);
 
+    fd1 = dup(fd);
+
+    printf("Descriptor using dup function\n");
+    printf("fd: %d\n", fd);
+    printf("fd1: %d\n", fd1);
+    */
     exit(0);
 }
 
 /*mydup2: own implemenation of dup2 function */
-int mydpu2(int fd, int fd2)
+int mydup2(int oldfd, int newfd) 
 {
+    int max_fd, open_fd[newfd];
+    int num_of_fd = 0;
+    
+    struct stat s;
 
-    /* don't forget to close previous fd2 file descriptor */
+    /* Check if oldfd is a valid file descriptor */
+    if (fstat(oldfd, &s) < 0) {
+        errno = EBADF;
+        return -1;
+    }
 
+    /*Check newfd is in valid range*/
+    max_fd = sysconf(_SC_OPEN_MAX);
+    //printf("max_fd: %d\n", max_fd);
+    if (newfd < 0 || newfd > max_fd)
+    {
+        errno = EBADF;
+        return -1;
+    }
+
+    /*If oldfd is equal newfd, return oldfd */
+    if (newfd == oldfd)
+        return newfd;
+
+    /* new newfd is open close it */
+    if(fstat(newfd, &s) == 0)
+        close(newfd);
+
+
+    /* In loop duplicate file descriptors using dup till we get to newfd.
+       Then close all opened fds and return newfd  */
+    while (1)
+    {
+        if ((max_fd = dup(oldfd)) == -1) {
+            errno = EBADF;
+            return -1;
+        }
+        if (max_fd == newfd) {
+            /* close all previously opened fds */
+            while (--num_of_fd >= 0)
+                close(open_fd[num_of_fd]);
+            return max_fd;
+        }
+        else /* store opened fd */ 
+            open_fd[num_of_fd++] = max_fd;
+        
+    }
 }
